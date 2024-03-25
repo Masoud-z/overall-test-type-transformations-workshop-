ebac7f1c9cce85d0b1d13d2bc001667dd796c1e3
Masoud <masoud.zeinalabedini@gmail.com>
2024-03-25 14:08:30 +0330

    Finish all of it

--- src/06-challenges/38-mutually-exclusive-properties.problem.ts
+++ src/06-challenges/38-mutually-exclusive-properties.problem.ts
@@ -10,7 +10,9 @@ interface Attributes {
  * How do we create a type helper that represents a union
  * of all possible combinations of Attributes?
  */
-type MutuallyExclusive<T> = unknown;
+type MutuallyExclusive<T> = {
+  [K in keyof Attributes]: Record<K, Attributes[K]>;
+}[keyof Attributes];
 
 type ExclusiveAttributes = MutuallyExclusive<Attributes>;
 
@@ -28,5 +30,5 @@ type tests = [
           username: string;
         }
     >
-  >,
+  >
 ];
